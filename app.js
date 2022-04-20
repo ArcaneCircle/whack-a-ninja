9ade476bf9e260b8f5752cb185c57c09aeb86d84
M4ss1ck <4ndyraul@gmail.com>
2022-04-19 23:44:56 -0400

    getting ready for scoreboard

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 const square = document.querySelectorAll(".square");
 const ninja = document.querySelectorAll(".ninja");
 const timeLeft = document.querySelector("#time-left");
+const button = document.querySelector("#start");
 let score = document.querySelector("#score");
 
 let result = 0;
 let currentTime = timeLeft.textContent;
+let running = false;
+let timerIdCountdown; // = setInterval(countDown, 1000);
+const player = "Tú"; //window.webxdc.selfName;
 
 function randomSquare() {
   square.forEach((className) => {
     className.classList.remove("ninja");
   });
-  let randomPosition = square[Math.floor(Math.random() * 9)];
-  randomPosition.classList.add("ninja");
-  //assign the id of the randomPosition to hitPosition for us to use later
-  hitPosition = randomPosition.id;
+  if (running) {
+    let randomPosition = square[Math.floor(Math.random() * 9)];
+    randomPosition.classList.add("ninja");
+    //assign the id of the randomPosition to hitPosition for us to use later
+    hitPosition = randomPosition.id;
+  }
 }
 
 square.forEach((id) => {
@@ -21,6 +27,10 @@ square.forEach((id) => {
     if (id.id === hitPosition) {
       result = result + 1;
       score.textContent = result;
+      // hardcore mode below
+      // } else {
+      //   result = result - 1;
+      //   score.textContent = result;
     }
   });
 });
@@ -30,16 +40,33 @@ function moveNinja() {
   timerId = setInterval(randomSquare, 500);
 }
 
-moveNinja();
-
 function countDown() {
   currentTime--;
   timeLeft.textContent = currentTime;
 
   if (currentTime === 0) {
-    clearInterval(timerId);
+    clearInterval(timerIdCountdown);
     alert("JUEGO TERMINADO. Tu puntuación fue de " + result + " puntos");
   }
 }
 
-let timerId = setInterval(countDown, 1000);
+function start() {
+  running = true;
+  timerIdCountdown = setInterval(countDown, 1000);
+  moveNinja();
+}
+
+function stop() {
+  //clearInterval(timerId);
+  clearInterval(timerIdCountdown);
+  running = false;
+  currentTime = 60;
+  timeLeft.textContent = currentTime;
+  alert(
+    "JUEGO TERMINADO.\n" +
+      player +
+      ", tu puntuación fue de " +
+      result +
+      " puntos"
+  );
+}
