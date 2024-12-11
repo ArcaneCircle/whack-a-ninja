68bcf932fb720c4c7875ca840b0d244d634fd36a
adbenitez <asieldbenitez@gmail.com>
2024-12-11 14:26:18 +0100

    update deps

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+import "@webxdc/highscores";
+
 const square = document.querySelectorAll(".square");
 const ninja = document.querySelectorAll(".ninja");
 const timeLeft = document.querySelector("#time-left");
@@ -127,28 +129,8 @@ function stop() {
   currentTime = 60;
   hitPosition = null;
   timeLeft.textContent = currentTime;
-  const addr = window.webxdc.selfAddr;
-  const name = window.webxdc.selfName;
-  if (highscore(addr) < result) {
-    const amount = result === 1 ? "1 point" : result + " points";
-    const info = name + " scored " + amount + " in Whack-A-Ninja!";
-    const payload = { addr: addr, name: name, score: result };
-    updateHighscore(addr, name, result);
-    window.webxdc.sendUpdate({ payload: payload, info: info }, info);
-  }
+  window.highscores.setScore(result)
   showGameOver(result);
-}
-
-// modified from https://github.com/adbenitez/StackUp.xdc/blob/master/js/index.js
-
-async function updateLoader() {
-  window.webxdc.setUpdateListener((update) => {
-    const player = update.payload;
-    updateHighscore(player.addr, player.name, player.score);
-    if (update.serial === update.max_serial) {
-      lugares.style.display = "flex";
-    }
-  });
 }
 
 function showGameOver(result) {
@@ -159,58 +141,6 @@ function showGameOver(result) {
   gameover.style.display = "block";
 }
 
-function showScoreboard() {
-  const container = document.getElementById("scoreboard-container");
-  container.innerHTML = "";
-  const addr = window.webxdc.selfAddr;
-  const list = document.createElement("ol");
-  list.className = "w3-ol";
-  highscores().forEach((item) => {
-    const name = document.createElement("span");
-    name.className = "w3-large";
-    name.textContent =
-      item.name.length > 20 ? item.name.substring(0, 20) + "…" : item.name;
-    const score = document.createElement("span");
-    score.textContent = item.score;
-    score.className = "w3-right";
-    const li = document.createElement("li");
-    if (item.addr == addr) {
-      const strong = document.createElement("strong");
-      strong.appendChild(name);
-      strong.appendChild(score);
-      li.appendChild(strong);
-    } else {
-      li.appendChild(name);
-      li.appendChild(score);
-    }
-    list.appendChild(li);
-  });
-  container.appendChild(list);
-  document.getElementById("scoreboard").style.display = "block";
-}
-
-function updateHighscore(addr, name, score) {
-  if (highscore(addr) < score) {
-    PLAYERS[addr] = { name: name, score: score };
-  }
-}
-
-function highscore(addr) {
-  return PLAYERS[addr] ? PLAYERS[addr].score : 0;
-}
-
-function highscores() {
-  return Object.keys(PLAYERS)
-    .map((addr) => {
-      const player = PLAYERS[addr];
-      player.addr = addr;
-      return player;
-    })
-    .sort((a, b) => b.score - a.score);
-}
-
-let PLAYERS = {};
-
 function clearContainer(container) {
   while (container.firstChild) {
     container.removeChild(container.firstChild);
@@ -219,5 +149,14 @@ function clearContainer(container) {
 
 
 window.toggle = toggle;
-window.showScoreboard = showScoreboard;
-updateLoader();
+window.showScoreboard = () => {
+  const container = document.getElementById("scoreboard-container");
+  container.innerHTML = window.highscores.renderScoreboard().innerHTML;
+  document.getElementById("scoreboard").style.display = "block";
+};
+
+window.highscores.init({
+  onHighscoresChanged: () => {
+    lugares.style.display = "flex";
+  }
+});
